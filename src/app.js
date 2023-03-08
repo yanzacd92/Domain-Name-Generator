c85540c6f79f53ce5d9d6f55a02ad4b7359e08c4
yanzacd92 <yanzacd@gmail.com>
2023-03-08 22:32:53 +0000

    added domains

--- src/app.js
+++ src/app.js
@@ -10,8 +10,11 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let domains2 = pronoun.flatMap(p => adj.map(a => noun.map(n => p + a + n)));
-  let result = domains2.toString().replaceAll(",", ".com <br/>");
+  let domain = [".com", ".net", ".us", ".io"];
+  let domains2 = pronoun.flatMap(p =>
+    adj.map(a => noun.map(n => domain.map(d => p + a + n + d)))
+  );
+  let result = domains2.toString().replaceAll(",", "<br/>");
   console.log(result);
   document.getElementById("combinations").innerHTML = result;
 };
